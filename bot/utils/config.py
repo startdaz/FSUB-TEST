a8d1b5e37d39607f0593ddfcb41fd95fc06cdee4
Rans33281 <53783641+Rans33281@users.noreply.github.com>
2024-10-21 19:53:56 +0700

    config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 1234))
-    API_HASH: str = os.environ.get("API_HASH", "b184")
-    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7u9jl")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 4879))
-    MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://root:passwd@mongo")
-    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID" -100))
+    API_ID = int(os.environ.get("API_ID", 1234)) #GANTI "1234" DENGAN API_ID
+    API_HASH: str = os.environ.get("API_HASH", "b184") #GANTI "b184" DENGAN API_HASH , JANGAN HAPUS TANDA (" ") NYA !!!
+    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7u9jl") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1234)) #GANTI "1234" DENGAN ID PEMILIK BOT
+    MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://root:passwd@mongo") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
+    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID" -100)) #GANTI DENGAN DATABASE CHANNEL
 
 
 config: "Config" = Config()
