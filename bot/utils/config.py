41f8f5137fde22c06ba50fc580fa33b51e98cfbc
Rans33281 <53783641+Rans33281@users.noreply.github.com>
2024-10-21 19:42:52 +0700

    config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 2040))
-    API_HASH: str = os.environ.get("API_HASH", "b18441a1ff607e10a989891a5462e627")
-    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 487936750))
+    API_ID = int(os.environ.get("API_ID", 1234))
+    API_HASH: str = os.environ.get("API_HASH", "b184")
+    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7u9jl")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 4879))
     MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://root:passwd@mongo")
-    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID"))
+    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID" -100))
 
 
 config: "Config" = Config()
