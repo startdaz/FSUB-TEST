b82e326a45a94db4e16fddf0a86bf4aec24d69ec
Rans33281 <53783641+Rans33281@users.noreply.github.com>
2024-11-14 08:48:36 +0700

    config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7u9jl") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
     OWNER_ID = int(os.environ.get("OWNER_ID", 1234)) #GANTI "1234" DENGAN ID PEMILIK BOT
     MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://root:passwd@mongo") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
-    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID" -100)) #GANTI DENGAN DATABASE CHANNEL
+    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", -100)) #GANTI DENGAN DATABASE CHANNEL
 
 
 config: "Config" = Config()
