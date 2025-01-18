00f3bcdd97c52fc9e957454846bbd74bfcad755f
iniemin <53783641+iniemin@users.noreply.github.com>
2025-01-18 09:44:04 +0700

    [+]

--- plugins/stats.py
+++ plugins/stats.py
@@ -7,7 +7,7 @@ from bot import authorized_users_only, get_users
 @Client.on_message(filters.private & filters.command("users"))
 @authorized_users_only
 async def users_handler(_, message: Message) -> None:
-    counting_message = await message.reply_text("Counting...", quote=True)
+    counting_message = await message.reply_text("ᴍᴇɴɢʜɪᴛᴜɴɢ...", quote=True)
 
     total_users = await get_users()
     await counting_message.edit_text(f"<code>{len(total_users)}</code> users")
