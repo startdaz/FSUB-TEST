c9d1f6741aa7f4db7ba14f2030632d2b90c28266
TreNor's .ID <128129543+startdaz@users.noreply.github.com>
2025-02-23 20:40:50 +0700

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -1,36 +1,23 @@
-import os
+import re
+import sys
+from os import getenv
+
 from dotenv import load_dotenv
+from pyrogram import filters
 
-# Muat variabel dari file .env
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Ambil path FSUB-TEST
-ENV_PATH = os.path.join(BASE_DIR, ".env")
-load_dotenv(ENV_PATH, override=True)  # Pastikan variabel di .env digunakan
+load_dotenv()
 
-class Config:
-    API_ID = int(os.getenv("API_ID")) if os.getenv("API_ID") else None
-    API_HASH = os.getenv("API_HASH") or ""
-    BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
-    OWNER_ID = int(os.getenv("OWNER_ID")) if os.getenv("OWNER_ID") else None
-    MONGODB_URI = os.getenv("MONGODB_URI") or ""
-    DATABASE_CHAT_ID = int(os.getenv("DATABASE_CHAT_ID")) if os.getenv("DATABASE_CHAT_ID") else None
-
-config = Config()
-BOT_ID = config.BOT_TOKEN.split(":", 1)[0] if config.BOT_TOKEN else None
-
-
-"""
 import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 1234)) #GANTI "1234" DENGAN API_ID
-    API_HASH: str = os.environ.get("API_HASH", "b184") #GANTI "b184" DENGAN API_HASH , JANGAN HAPUS TANDA (" ") NYA !!!
-    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7u9jl") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1234)) #GANTI "1234" DENGAN ID PEMILIK BOT
-    MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://root:passwd@mongo") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
-    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", -100)) #GANTI DENGAN DATABASE CHANNEL
+    API_ID = int(os.environ.get("API_ID", "")) #GANTI "1234" DENGAN API_ID
+    API_HASH: str = os.environ.get("API_HASH", "") #GANTI "b184" DENGAN API_HASH , JANGAN HAPUS TANDA (" ") NYA !!!
+    BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
+    OWNER_ID = int(os.environ.get("OWNER_ID", "")) #GANTI "1234" DENGAN ID PEMILIK BOT
+    MONGODB_URI: str = os.environ.get("MONGODB_URI", "") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
+    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", "")) #GANTI DENGAN DATABASE CHANNEL
 
 
 config: "Config" = Config()
 BOT_ID = config.BOT_TOKEN.split(":", 1)[0]
-"""
